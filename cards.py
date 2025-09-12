9cceaf449a4788255728f1e6be84a4ecb77a78c3
Jeovani A. Quintanilla <jeovaniquintanilla35@gmail.com>
2025-09-11 20:07:38 -0400

    feat: create the Deck class

--- cards.py
+++ cards.py
@@ -19,6 +19,15 @@ class Card:
 
     def toString(self):
         return self.face + "_of_" + self.suit
+    
+
+class Deck:
+
+    def __init__(self):
+        self.faceName = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+        self.suit = ["hearts", "diamonds", "spades", "clubs"]
+        self.deck = []
+
 
 #main -- use to test code for file   
 s = Card("6","Clubs")
