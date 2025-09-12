a12d6b8b9d85bfee37e1d85200bb7db6f5f1537b
JeovaniQuintanilla <jeovaniquintanilla35@gmail.com>
2025-09-12 13:51:02 -0400

    feat: added siwtch statement to get result of choice

--- cards.py
+++ cards.py
@@ -76,18 +76,39 @@ if ans=="Y" or ans=="y":
     while attempt:
         fcard = d.pullACard()
         print(f"The first card is -> {fcard.toString()}")
-        val1 = d.convertValue(fcard)
+        val1 = int(d.convertValue(fcard))
 
         choice = input("Is the next card higher, or lower??\n").lower()
         print(f"You chose -> {choice}.")
 
         sCard = d.pullACard()
-        val2 = d.convertValue(sCard)
+        val2 = int(d.convertValue(sCard))
         print(f"Second card is -> {sCard.toString()}")
 
-     
+        match choice:
+            case "higher":
+                if val1 < val2:
+                    count+=1
+                    print(f"You were right, it was higher, point earned -> Score:{count}")
+                   
+                elif val1 == val2:
+                    count+=1
+                    print(f"Lucky, they were the same, I'll give you a point -> Score:{count}")
+                else: 
+                    print("Wrong answer, Better luck next time, by bye.")
+                    attempt = False
 
-
+            case "lower":
+                if val1 > val2:
+                    count+=1
+                    print(f"You were right, it was lower, point earned -> Score:{count}")
+                
+                elif val1 == val2:
+                    count+=1
+                    print(f"Lucky, they were the same, I'll give you a point -> Score:{count}")
+                else:
+                    print("Wrong answer, Better luck next time, bye bye.")
+                    attempt = False
 else:
     print("Goodbye, maybe next time.")     
     
