1a8f95ce4447860fc5234a8eb2f044dea4442b9b
JeovaniQuintanilla <jeovaniquintanilla35@gmail.com>
2025-09-15 23:55:53 -0400

    feat: added customtkinter

--- Higher_or_Lower_GUI.py
+++ Higher_or_Lower_GUI.py
@@ -1,3 +1,6 @@
 #This file contains the GUI versions of the Higher or Lower Game
+import customtkinter
+from tkinter import *
+from PIL import Image, ImageTk
 
 print("Welcome to the GUI verion of the game")
