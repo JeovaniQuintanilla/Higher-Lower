747026911fc1b04bf7b327b8d16d4d32bdeedbfc
JeovaniQuintanilla <jeovaniquintanilla35@gmail.com>
2025-09-15 23:56:34 -0400

    feat: created app window

--- Higher_or_Lower_GUI.py
+++ Higher_or_Lower_GUI.py
@@ -4,3 +4,7 @@ from tkinter import *
 from PIL import Image, ImageTk
 
 print("Welcome to the GUI verion of the game")
+
+root = customtkinter.CTk()
+root.geometry("800x700")
+root.title("Higher & Lower")
