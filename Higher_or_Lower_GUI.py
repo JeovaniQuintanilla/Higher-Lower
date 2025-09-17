dc8c692f34f6e780673f11c5ab8d45a3de403098
JeovaniQuintanilla <jeovaniquintanilla35@gmail.com>
2025-09-17 00:03:59 -0400

    feat: create a dummy method to test mechanics

--- Higher_or_Lower_GUI.py
+++ Higher_or_Lower_GUI.py
@@ -2,6 +2,7 @@
 import customtkinter
 from tkinter import *
 from PIL import Image, ImageTk
+import random
 
 print("Welcome to the GUI verion of the game")
 
@@ -9,18 +10,23 @@ root = customtkinter.CTk()
 root.geometry("800x700")
 root.title("Higher & Lower")
 
+def test_function(event):
+    num = random.randrange(10)
+    label.configure(text=f"{num}")
+    print(num)
+
 
 #This is the player interaction
 label = customtkinter.CTkLabel(root, text="Welcome to Higher or Lower!")
 submit = customtkinter.CTkEntry(root, placeholder_text= "Enter your Guess..")
 sBtn = customtkinter.CTkButton(root)
+sBtn.bind('<Button-1>',test_function)
 
 #Score Area
 score = customtkinter.CTkLabel(root, text="Score:")
 score.place(x=680, y=625)
 high_score = customtkinter.CTkLabel(root, text="High-Score:")
 high_score.place(x=650, y=600)
-
 
 label.pack(),submit.pack(),sBtn.pack()
 root.mainloop()
